12aaa9b86aa1975f1ae612cd6f6760a505c3dacd
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-17 11:32:09 -0600

    Test .min file

--- node-test/main.test.mjs
+++ node-test/main.test.mjs
@@ -12,4 +12,14 @@ describe("@esm-bundle/styled-components", () => {
     expect(keyframes).not.to.equal(undefined);
     expect(styled.keyframes).to.equal(undefined);
   });
+
+  it("has API exports defined", async () => {
+    const { default: styled, keyframes } = await import(
+      "../esm/styled-components.min.js"
+    );
+
+    expect(styled.div).not.to.equal(undefined);
+    expect(keyframes).not.to.equal(undefined);
+    expect(styled.keyframes).to.equal(undefined);
+  });
 });
