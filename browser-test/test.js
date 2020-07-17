9e881892626f11f5e37bf5475822bb88b69148a9
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-17 11:33:23 -0600

    Test .min file

--- browser-test/test.js
+++ browser-test/test.js
@@ -15,4 +15,17 @@ describe("@esm-bundle/styled-components System.register import", () => {
       expect(typeof module.keyframes).toBe("function");
     });
   });
+
+  it("can load", () => {
+    return System.import("/base/system/styled-components.min.js").then(
+      (module) => {
+        expect(module.default).toBeDefined();
+        expect(module.__esModule).toEqual(true);
+        // Default export
+        expect(typeof module.default.div).toBe("function");
+        // Named export
+        expect(typeof module.keyframes).toBe("function");
+      }
+    );
+  });
 });
