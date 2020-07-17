a5fd7f1c79e8e6083b0890f6680b44b312d023af
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-17 09:48:39 -0600

    Update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,43 @@
+import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
 
-function createConfig(format) {
+function createConfig(format, resolved = false) {
   const dir = format === "module" ? "esm" : format;
+
   return {
-    input: require.resolve("autopublish-template"),
+    input: `src/index.js`,
     output: {
-      file: `${dir}/index.js`,
+      file: `${dir}/index${resolved ? ".resolved" : ""}.js`,
       sourcemap: true,
       format,
+      paths: {
+        react: resolved
+          ? `//cdn.jsdelivr.net/npm/@esm-bundle/react@16.13.1/esm/react.production.min.js`
+          : "react",
+        "react-is": resolved
+          ? "//cdn.jsdelivr.net/npm/@esm-bundle/react-is@16.13.1/esm/react-is.production.min.js"
+          : "react-is",
+      },
+      exports: "named",
     },
-    plugins: [commonjs(), terser()],
+    plugins: [
+      resolve(),
+      commonjs(),
+      replace({
+        values: {
+          "process.env.NODE_ENV": JSON.stringify("production"),
+        },
+      }),
+      terser(),
+    ],
+    external: ["react-is", "react"],
   };
 }
 
-export default [createConfig("module"), createConfig("system")];
+export default [
+  createConfig("module"),
+  createConfig("module", true),
+  createConfig("system"),
+];
