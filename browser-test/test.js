f879db0ed83c70c967d93e93e6af9fbc66c68701
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-17 09:49:35 -0600

    Update tests

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,9 +1,16 @@
-describe("@esm-bundle/autopublish-template", () => {
+describe("@esm-bundle/styled-components", () => {
   it("can load the ESM bundle", () => {
-    return import("/base/esm/index.js");
+    return import("/base/esm/index.resolved.js");
   });
 
   it("can load the System.register bundle", () => {
-    return System.import("/base/system/index.js");
+    return System.import("/base/system/index.js").then((module) => {
+      expect(module.default).toBeDefined();
+      expect(module.__esModule).toEqual(true);
+      // Default export
+      expect(typeof module.default.div).toBe("function");
+      // Named export
+      expect(typeof module.keyframes).toBe("function");
+    });
   });
 });
--- node-test/main.test.mjs
+++ node-test/main.test.mjs
@@ -1,5 +1,13 @@
-describe("@esm-bundle/autopublish-template", () => {
+describe("@esm-bundle/styled-components", () => {
   it("can load the esm bundle without dying", () => {
     return import("../esm/index.js");
+  });
+
+  it("has API exports defined", async () => {
+    const { default: styled, keyframes } = await import("../esm/index.js");
+
+    expect(styled.div).not.to.equal(undefined);
+    expect(keyframes).not.to.equal(undefined);
+    expect(styled.keyframes).to.equal(undefined);
   });
 });
