a3f564c62f57e3e40d990490a5d54cd0db4a6e4e
Carlos Filoteo <c.filoteo12@gmail.com>
2020-09-08 23:28:03 -0600

    Use non-browser esm for base build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Using the browser build that removes the need for node streams (for server rendering)
-import styled from "styled-components/dist/styled-components.browser.esm.js";
+import styled from "styled-components/dist/styled-components.esm.js";
 export default styled;
-export * from "styled-components/dist/styled-components.browser.esm.js";
+export * from "styled-components/dist/styled-components.esm.js";
 export const __esModule = true;
